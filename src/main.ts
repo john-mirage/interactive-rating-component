b3d074b32834942eae5782874967fa88d41cafc7
john-mirage <chaosdynamix@posteo.net>
2022-03-23 18:19:28 +0100

    fix: disable inputs when rating is submitted

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,26 @@
 import "./assets/styles/index.css";
+
+const ratingSubmitInput = document.getElementById("rating-submit");
+const ratingInputs = document.getElementsByClassName("rating-inputs");
+
+let inputsAreDisabled = false;
+
+function disableInputs() {
+    if (!inputsAreDisabled) {
+        for (let index = 0; index < ratingInputs.length; index++) {
+            ratingInputs[index].disabled = true;
+        }
+        ratingSubmitInput.disabled = true;
+        inputsAreDisabled = true;
+    }
+}
+
+ratingSubmitInput?.addEventListener("keyup", (event) => {
+    event.preventDefault();
+    if (event.code === "Enter") {
+        event.target.checked = true;
+        disableInputs();
+    }
+});
+
+ratingSubmitInput?.addEventListener("change", disableInputs);
