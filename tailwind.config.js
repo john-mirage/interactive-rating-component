41f60f568ecaae7ad327e696f107084ac0111605
john-mirage <chaosdynamix@posteo.net>
2022-03-23 14:28:16 +0100

    feat: add custom tailwind values and polish ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,18 +7,35 @@ module.exports = {
   ],
   theme: {
     screens: {
-      ratings: '416px',
+      ratings: "360px",
+      sm: "576px",
     },
     colors: {
       "orange": "hsl(25, 97%, 53%)",
       "white": "hsl(0, 0%, 100%)",
+      "light-grey": "hsl(217, 12%, 63%)",
       "medium-grey": "hsl(216, 12%, 54%)",
       "dark-blue": "hsl(213, 19%, 18%)",
+      "button": "#262F38",
+      "background-from": "#171E28",
+      "background-to": "#1E252F",
       "very-dark-blue": "hsl(216, 12%, 8%)",
-      "button": "rgba(255,255,255,0.05)",
       "transparent": "transparent",
     },
     extend: {
+      spacing: {
+        "star": "18px",
+        "ratings": "calc(100% + 1rem)",
+      },
+      borderRadius: {
+        "4xl": "1.5rem",
+      },
+      letterSpacing: {
+        "button": "2px",
+      },
+      maxWidth: {
+        "card": "412px",
+      },
       fontFamily: {
         sans: [
           'Overpass',
